ef65113243fe16e3ae5ad0171f2ba516a9518daf
ashish111333 <ashishkumar0265aaa@gmail.com>
2025-11-26 04:54:18 +0530

    cache folder
    Signed-off-by: ashish111333 <ashishkumar0265aaa@gmail.com>

---
+++ src/python/cache/cache.py
@@ -0,0 +1,49 @@
+# LRU cache and MRU cache 
+
+from typing import Any,Dict
+
+class CacheItem:
+    def __init(self,value: Any,ttl: int):
+        self.value=value
+        self.ttl=ttl
+    
+
+# doubly linked list        
+class Dll:
+    size=0
+    def __init__(self):
+        self.head=None
+        self.tail=None
+    
+    def insert(self,data: Any):
+        pass
+         
+    def delete():
+        pass
+    @property
+    def is_empty(self)->bool:
+        if not self.size:
+            return True
+    
+            
+            
+    
+    
+    
+class Node:
+    def __init__(self,data: Any):
+        self.data=data
+        self.l_ptr: Node=None
+        self.r_ptr: Node=None
+        
+class LruCache:
+    ignore_ttl=True
+    default_size=10
+    size=0
+    def __init__(self,size: int | None):
+        self.cache_data: Dict[any,CacheItem]={}
+        self.size=size
+    
+
+       
+    
